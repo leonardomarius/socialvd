d80acdd93542b3c670ae888ea822fd2d46a4622a
leonardomarius <leobiziere@gmail.com>
2025-11-28 20:42:45 +0100

    ajout pour speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,11 +4,13 @@ import AuthProvider from "@/components/AuthProvider";
 import PageTransitionLoader from "@/components/PageTransitionLoader";
 import { Inter_Tight } from "next/font/google";
 
+// 👉 AJOUT
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 const interTight = Inter_Tight({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700"],
 });
-
 
 export const metadata = {
   title: "SocialVD",
@@ -17,8 +19,7 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="eng" className={interTight.className}>
-      
+    <html lang="en" className={interTight.className}>
       <body style={{ minHeight: "100vh", margin: 0 }}>
         {/* Gestion session Supabase */}
         <AuthProvider>
@@ -38,6 +39,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             </main>
           </PageTransitionLoader>
         </AuthProvider>
+
+        {/* 👉 AJOUT : SpeedInsights */}
+        <SpeedInsights />
+
       </body>
     </html>
   );
