d9c21c08961963f1ed99eb55f0efdd536ed4548f
leonardomarius <leobiziere@gmail.com>
2025-12-03 18:28:06 +0100

    remove nested old folder

--- socialvd/src/app/messages/create/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Suspense } from "react";
-import CreateConversationClient from "../create/CreateConversationClient";
-export default function Page() {
-  return (
-    <Suspense fallback={<p>Chargement...</p>}>
-      <CreateConversationClient />
-    </Suspense>
-  );
-}
